d2e3b5508c0b8fcdd6f359dad8bfbb6fe19e84b5
ekapituro <ekapituro@gmail.com>
2023-08-09 08:18:53 +0200

    Update

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="head-text head-left">Home</h1>
+    </div>
+  )
+}
