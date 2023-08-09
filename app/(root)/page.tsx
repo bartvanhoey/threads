a7221e08d97b7aa567fea1018973ddbbb21b2ef1
ekapituro <ekapituro@gmail.com>
2023-08-09 21:04:32 +0200

    Update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 // "use client";
 
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchThreads } from "@/lib/actions/thread.actions";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs";
@@ -11,24 +12,29 @@ export default async function Home() {
   console.log("results:", result);
 
   return (
-    <div>
+    <>
       <h1 className='head-text head-left'>Home</h1>
-      <section className="mt-9 flex flex-col gap-10" >
-        {result.threads.length === 0 ? (<p className="text-center !text-base-regular text-light-3"></p>) : (
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.threads.length === 0 ? (
+          <p className='text-center !text-base-regular text-light-3'></p>
+        ) : (
           <>
-          {result.threads.map((thread) => (<ThreadCard 
-            key={thread._id} 
-            id={thread._id} 
-            currentUserId={user?.id} 
-            parentId={thread.parentId} 
-            content={thread.text}
-            author={thread.author}
-            community={thread.community}
-            createdAt={thread.createdAt}
-            comments={thread.children}  />)}
+            {result.threads.map((thread) => (
+              <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id || ""}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
           </>
         )}
       </section>
-    </div>
+    </>
   );
 }
